a874a4410603ad6f557b622f88be19418c53059c
Simon Marache-Francisco <web@marache.net>
2016-05-31 21:32:05 +0100

    upgraded pyinstaller version

--- compiler.bat
+++ compiler.bat
@@ -1,3 +1,2 @@
-REM c:\Python25\python.exe pyinstaller-1.4\Makespec.py --onefile --tk --noconsole  photosTroglo.py
-c:\Python25\python.exe pyinstaller-1.4\Build.py   photosTroglo.spec
+pyinstaller photosTroglo.spec
 choice
